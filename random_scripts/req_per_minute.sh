ca05dac85c18adc6f74a9f37485d2ed99a3c2d88
Grzegorz Adamowicz <grzegorz.adamowicz@kaufda.de>
2016-01-05 12:12:13 +0100

    Remove random_scirpts -> moved to https://github.com/gstlt/log_analysis

--- random_scripts/req_per_minute.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-filename=$1
-
-awk '{print $4}' ${filename} | cut -d: -f2-3 | uniq -c
--- random_scripts/top_unsuccessful.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ue
-
-filename=$1
-
-awk '{print $9 "\tRequest: " $7}' ${filename} | grep -v '^200' | sort | uniq -c
-
